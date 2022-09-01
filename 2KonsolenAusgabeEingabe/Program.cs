1d65c6febffa0c5c89d15819b9e11750465ec3ff
Marcel Kroschke <marcel.k48@gso.schule.koeln>
2022-09-01 10:44:45 +0200

    Aufgaben Bearbeitet und Fertig Gestellt

---
+++ 2KonsolenAusgabeEingabe/Program.cs
@@ -0,0 +1,34 @@
+﻿//Aufgabe1
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+//Aufgabe2
+string Vorname;
+string Nachname;
+int Alter;
+
+Vorname = "Marcel";
+Nachname = "Kroschke";
+Alter = 17;
+
+Console.WriteLine($"{Vorname}");
+Console.WriteLine($"{Nachname}");
+Console.WriteLine($"{Alter}");
+
+//Aufgabe3
+string Vorname1;
+string Alter1;
+string Klasse1;
+
+
+Console.WriteLine("Name?");
+Vorname1 = Console.ReadLine();
+Console.WriteLine("Alter?");
+Alter1 = Console.ReadLine();
+Console.WriteLine("klasse?");
+Klasse1 = Console.ReadLine();
+
+Console.WriteLine($"{Vorname1} ist {Alter1} Jahre alt und besucht die {Klasse1} des Georg-Simon-Ohm Berufskollegs");
